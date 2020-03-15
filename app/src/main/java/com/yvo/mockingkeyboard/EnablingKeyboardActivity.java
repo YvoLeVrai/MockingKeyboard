9498ef0bc20cb96f4f1413611d03339b37d2174f
Yvo <momocraft49@gmail.com>
2020-03-15 08:43:01 +0100

    Added activity to enable keyboard

---
+++ app/src/main/java/com/yvo/mockingkeyboard/EnablingKeyboardActivity.java
@@ -0,0 +1,62 @@
+package com.yvo.mockingkeyboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodInfo;
+import android.view.inputmethod.InputMethodManager;
+
+import java.util.List;
+
+public class EnablingKeyboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isKeyboardEnabled())
+        {
+            setContentView(R.layout.activity_ennabled_keyboard);
+        }
+        else
+        {
+            setContentView(R.layout.activity_enabling_keyboard);
+        }
+    }
+
+    public void button_enable_Click(View view)
+    {
+        //Entrer dans les options de l'app
+        Intent intent = new Intent(Settings.ACTION_INPUT_METHOD_SETTINGS);
+        startActivity(intent);
+    }
+
+    private boolean isKeyboardEnabled()
+    {
+        boolean enabled = false;
+
+        InputMethodManager imeManager = (InputMethodManager)getApplicationContext().getSystemService(INPUT_METHOD_SERVICE);
+        List<InputMethodInfo> InputMethods = imeManager.getEnabledInputMethodList();
+
+        Log.i("Keyboard List", InputMethods.toString());
+
+        for (InputMethodInfo inputMethodInfo : InputMethods)
+        {
+            Log.i("InputMethodInfo", inputMethodInfo.getPackageName());
+            if (inputMethodInfo.getPackageName().equals("com.yvo.mockingkeyboard"))
+            {
+                enabled = true;
+            }
+        }
+        Log.i("enabled", String.valueOf(enabled));
+        return enabled;
+    }
+}
